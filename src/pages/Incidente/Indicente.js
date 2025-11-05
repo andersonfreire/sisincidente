a5515bb122b123af05dc983bdb4ebe0e3a877e63
Joan Medeiros <joanmedeiros2018@gmail.com>
2025-11-05 16:28:06 -0300

    adicionando o componente Accordion na page Incidente

--- src/pages/Incidente/Indicente.js
+++ src/pages/Incidente/Indicente.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Container, Card, Accordion } from "react-bootstrap";
 import IncidenteList from "./IncidenteList";
 import IncidenteForm from "./IncidenteForm";
 
@@ -13,22 +13,51 @@ const Incidente = () => {
     };
 
     return (
-        <Container className="m-0 p-0">
-            <Card className="border-0">
-                <Card.Body>
-                    <IncidenteForm
-                        selectedIncidente={selectedIncidente}
-                        setSelectedIncidente={setSelectedIncidente}
-                        onSave={handleSave}
-                    />
+            <Card className="border-0 m-0 p-0">
+                <Card.Body className="p-0">
+                    <Accordion defaultActiveKey="0" alwaysOpen className="d-flex flex-column gap-3">
+                        
+                        <Accordion.Item 
+                            eventKey="0" 
+                            style={{
+                                border: "none",
+                                boxShadow: "none"
+                            }}>
+                            <Accordion.Header>
+                                {selectedIncidente
+                                ? `✏️ Editar Incidente #${selectedIncidente.numeroChamado || ""}`
+                                : "➕ Novo Incidente"}
+                            </Accordion.Header>
+                            <Accordion.Body>
+                                <IncidenteForm
+                                    selectedIncidente={selectedIncidente}
+                                    setSelectedIncidente={setSelectedIncidente}
+                                    onSave={handleSave}
+                                />
+                            </Accordion.Body>
+                        </Accordion.Item>
 
-                    <IncidenteList
-                        key={refresh}
-                        onEdit={(i) => setSelectedIncidente(i)}
-                    />
+                        <Accordion.Item 
+                            eventKey="1" 
+                            style={{
+                                border: "none",
+                                boxShadow: "none"
+                            }}
+                        >
+                            <Accordion.Header>📋 Incidentes Registrados</Accordion.Header>
+                            <Accordion.Body>
+                                <IncidenteList
+                                    key={refresh}
+                                    onEdit={(i) => {
+                                        setSelectedIncidente(i);
+                                        window.scrollTo({ top: 0, behavior: "smooth" });
+                                    }}
+                                />
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
                 </Card.Body>
             </Card>
-        </Container>
     );
 };
 
